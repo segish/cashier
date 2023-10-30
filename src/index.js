adf3079eceeef1408995064b288ea3f6f7874695
Tsega Tigneh <136186978+segish@users.noreply.github.com>
2023-10-31 02:36:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import Axios from 'axios';
 import { AuthContextProvider } from "./context/Context";
-Axios.defaults.baseURL = "api.besal10.com/api/";
+Axios.defaults.baseURL = "http://api.besal10.com/api/";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
